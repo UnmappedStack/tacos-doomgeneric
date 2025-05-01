f59311f7b94e41b3f59cd88349d420c5ecfeabdc
UnmappedStack <jake.steinburger0@gmail.com>
2025-05-01 20:17:46 +1000

    DG_GetKey

--- doomgeneric/doomgeneric_tacos.c
+++ doomgeneric/doomgeneric_tacos.c
@@ -2,15 +2,18 @@
 #include "m_argv.h"
 #include "doomgeneric.h"
 #include <stdio.h>
+#include <TacOS.h>
 #include <syscall.h>
 #include <sys/mman.h>
 
 typedef struct {
+    int fd;
     uint64_t bpp;
     uint64_t pitch;
     uint32_t *ptr;
 } Fb;
 
+int kb_fd;
 Fb fb = {0};
 
 void get_fb_info(uint64_t *pitchbuf, uint64_t *bppbuf) {
@@ -18,15 +21,21 @@ void get_fb_info(uint64_t *pitchbuf, uint64_t *bppbuf) {
 }
 
 void DG_Init() {
+    // Init framebuffer
     get_fb_info(&fb.pitch, &fb.bpp);
-    int fb_fd = open("/dev/fb0", 0, 0);
-    if (fb_fd < 0) {
+    fb.fd = open("/dev/fb0", 0, 0);
+    if (fb.fd < 0) {
         printf("Failed to open framebuffer device.\n");
         exit(-1);
     }
-    fb.ptr = mmap(NULL, 0, 0, 0, fb_fd, 0);
+    fb.ptr = mmap(NULL, 0, 0, 0, fb.fd, 0);
     printf("fb ptr = %p, pitch = %zu, bpp = %zu\n", fb.ptr, fb.pitch, fb.bpp);
-    close(fb_fd);
+    // Init keyboard
+    kb_fd = open("/dev/kb0", 0, 0);
+    if (kb_fd < 0) {
+        printf("Failed to open keyboard device.\n");
+        exit(-1);
+    }
 }
 
 // yoink thanks dcraft
@@ -47,9 +56,28 @@ uint32_t DG_GetTicksMs() {
     return ticks++;
 }
 
+unsigned char to_doom_key(Key key) {
+    switch (key) {
+    case KeyEnter:      return KEY_ENTER;
+    case KeyEscape:     return KEY_ESCAPE;
+    case KeyLeftArrow:  return KEY_LEFTARROW;
+    case KeyRightArrow: return KEY_RIGHTARROW;
+    case KeyUpArrow:    return KEY_UPARROW;
+    case KeyDownArrow:  return KEY_DOWNARROW;
+    case KeyControl:    return KEY_FIRE;
+    case KeySpace:      return KEY_USE;
+    case KeyShift:      return KEY_RSHIFT;
+    default:            return 'a' + key;
+    }
+}
+
 int DG_GetKey(int* pressed, unsigned char* doomKey) {
-    // TODO: this is a stub, always returns that there's no key press
-    return 0;
+    Key key;
+    if (read(kb_fd, &key, 1) < 0) return 0;
+    if (key == KeyNoPress) return 0;
+    *pressed = !(key & 0x80);
+    *doomKey = to_doom_key(key & 0x7f);
+    return 1;
 }
 
 void DG_SetWindowTitle(const char * title) {}
@@ -61,5 +89,7 @@ int main(int argc, char **argv) {
     for (;;) {
         doomgeneric_Tick();
     }
+    close(fb.fd);
+    close(kb_fd);
     return 0;
 }
